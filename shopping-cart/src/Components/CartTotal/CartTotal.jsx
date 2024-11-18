cfb55c8e8f08b5c33c8c3ff7ef94a1214b1a7a37
charuzain <charuabhizain@gmail.com>
2024-11-18 11:09:15 -0500

    Add & Style Cart Total & Contine Shopping Link

--- shopping-cart/src/Components/CartTotal/CartTotal.jsx
+++ shopping-cart/src/Components/CartTotal/CartTotal.jsx
@@ -1,12 +1,19 @@
-import { useSelector } from "react-redux"
+import { useSelector } from "react-redux";
+import './CartTotal.scss'
+import { Link } from "react-router-dom";
 
 const CartTotal = () => {
 
   const totalPrice = useSelector(state => state.cart.reduce((a, c) => a + (c.quantity * c.price), 0))
 
   return (
-    <div>Total Price: {totalPrice}</div>
-  )
+    <>
+      <div className="total">Sub Total : $ {totalPrice}</div>
+      <div className="total__link-container">
+        <Link to={'/'} className="total__link">Continue Shopping</Link>
+      </div>
+    </>
+  );
 }
 
 export default CartTotal
